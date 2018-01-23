5e3064ea0d4a66e953ef39e3f08dd524ea9315cc
omagdiel <omagdiel@gmail.com>
2018-01-22 19:14:28 -0700

    updated schema and skill.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180123021302) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
